468a1a6f33b88a96fa5ecf8859e71f82a6f7298b
mackliu <macklun@gmail.com>
2023-05-18 14:03:39 +0800

    建立需要的檔案及基本的版型外觀

---
+++ admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>南港展覽館接駁專車-系統管理</title>
+    <link rel="stylesheet" href="./bootstrap/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<?php include "header.php";?>
+
+
+<script src="./jquery-3.7.0.min.js"></script>
+<script src="./bootstrap/bootstrap.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,10 @@
+<div class="col-12 d-flex justify-content-between p-3">
+    <div class="p-2">
+        LOGO
+    </div>
+    <div class="p-2">
+        <button class="btn btn-light">系統管理</button>
+        <a href='login.php' class="btn btn-light">登入</a>
+        <button class="btn btn-light">登出</button>
+    </div>
+</div>
