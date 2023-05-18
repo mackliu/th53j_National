2fe338b17da9b293ceed69eb240f74bc88a23170
mackliu <macklun@gmail.com>
2023-05-18 14:05:02 +0800

    建立所需的資料庫及資料表，並完成登入檢查功能

---
+++ api/db.php
@@ -0,0 +1,5 @@
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=th53j_national";
+$pdo=new PDO($dsn,'root','');
+
+session_start();
--- login.php
+++ login.php
@@ -13,6 +13,11 @@
 
 <h1 class="text-center">網站管理-登入</h1>
 <form action="./api/login.php" class="form-group col-6 m-auto" method="post">
+    <?php
+        if(isset($_GET['error'])){
+            echo "<div class='text-center text-danger'>帳號或密碼錯誤</div>";
+        }
+    ?>
     <div class="form-group row">
         <label for="" class="col-2 col-form-label">帳號：</label>
         <input type="text" name='acc' class="col-10 form-control">
