e5648dbfdba882fb508dacf9278c135d72861c8a
mackliu <macklun@gmail.com>
2023-05-20 09:58:12 +0800

    建立初始環境及取得bootstrap等資料

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>南港展覽館接駁專車-路網圖</title>
+    <link rel="stylesheet" href="./bootstrap/bootstrap.css">
+
+</head>
+<body>
+
+
+
+
+
+<script src="./jquery/jquery.js"></script>
+<script src="./bootstrap/bootstrap.js"></script>
+</body>
+</html>
