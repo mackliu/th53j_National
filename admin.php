ed1d2998723a81a4e3481e5346ce18e959f9f2bc
mackliu <macklun@gmail.com>
2023-05-20 11:40:27 +0800

    建立後台功能的標題樣式

--- admin.php
+++ admin.php
@@ -9,30 +9,33 @@
 </head>
 <body>
 <?php include "header.php";?>
-<a href="?pos=bus">接駁車管理</a>
-<a href="?pos=station">站點管理</a>
+<div class="container mt-5">
+    <div class="border p-3">
+        <a class="btn btn-light active" href="?pos=bus">接駁車管理</a>
+        <a class="btn btn-light " href="?pos=station">站點管理</a>
+    </div>
+    <h1 class="border p-3 text-center my-3">接駁車管理 <button class="btn btn-success">新增</button></h1>
+    <?php
+    /*     if(isset($_GET['pos'])){
+            $pos=$_GET['pos'];
+        }else{
+            $pos='bus';
+        } */
 
-<?php
-/*     if(isset($_GET['pos'])){
-        $pos=$_GET['pos'];
-    }else{
-        $pos='bus';
-    } */
+        /* $pos=(isset($_GET['pos']))?$_GET['pos']:"bus"; */
 
-    /* $pos=(isset($_GET['pos']))?$_GET['pos']:"bus"; */
+        $pos=$_GET['pos']??'bus';
 
-    $pos=$_GET['pos']??'bus';
-
-    switch($pos){
-        case "bus":
-            include "admin_bus.php";
-        break;
-        case "station":
-            include "admin_station.php";
-        break;
-    }
-
-?>
+        switch($pos){
+            case "bus":
+                include "admin_bus.php";
+            break;
+            case "station":
+                include "admin_station.php";
+            break;
+        }
+    ?>
+</div>
 
 <script src="./jquery/jquery.js"></script>
 <script src="./bootstrap/bootstrap.js"></script>
