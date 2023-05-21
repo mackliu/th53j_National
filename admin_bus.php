26e645caa230dbd4b6ad017eb4f618e87cf0ac17
mackliu <macklun@gmail.com>
2023-05-21 08:26:05 +0800

    增加PHP及JS的程式碼文字註解

--- admin_bus.php
+++ admin_bus.php
@@ -9,14 +9,23 @@
          <td class="col-4">操作</td>
      </tr>
      <?php 
+
+        //建立取得所有接駁車的sql語法
         $sql="select * from `bus`";
+
+        //向資料表發出請求，並取回所有接駁車的資料
         $rows=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+        //使用迴圈foreach來將每一筆資料印出在頁面上
         foreach($rows as $row){
      ?>
      <tr>
          <td><?=$row['name'];?></td>
-         <td><?=mb_substr($row['minute'],-2);?>分鐘</td>
+         <td><?=$row['minute'];?>分鐘</td>
          <td>
+                                        <!-- 在按鈕標籤中直接觸發onclick事件，並在事件發生時執行：
+                                            1.使用ajax向後端取得資料(edit())
+                                            2.使用jquery來切換畫面上各區塊的顯示狀態 -->
              <button class="btn btn-warning" onclick="edit(<?=$row['id'];?>);$('.edit').show();$('.list,.add').hide()">編輯</button>
              <button class="btn btn-danger">刪除</button>
          </td>
@@ -36,10 +45,12 @@
     </div>
     <div class="row w-100">
         <label for="" class="col-2">已行駛時間(分鐘)</label>   
-        <input  type="number" name="minute" id="minute1" class='form-group form-control col-10'>
+        <input  type="number" name="minute" id="addMinute" class='form-group form-control col-10'>
     </div>
     <div class="row w-100">
         <input  type="submit" value="新增" class='col-12 btn btn-success my-1'>
+                                                                                <!-- 在按鈕標籤中直接觸發onclick事件，並在事件發生時執行：
+                                                                                     1.使用jquery來切換畫面上各區塊的顯示狀態 -->
         <input  type="button" value="回上頁" class='col-12 btn btn-secondary my-1'  onclick="$('.list').show();$('.edit,.add').hide()">
     </div>
     </form>
@@ -50,10 +61,12 @@
     <form action="./api/edit_bus.php" method="post">
     <div class="row w-100">
         <label for="" class="col-2">已行駛時間(分鐘)</label>   
-        <input  type="number" name="minute" id="minute2" class='form-group form-control col-10'>
+        <input  type="number" name="minute" id="editMminute" class='form-group form-control col-10'>
     </div>
     <div class="row w-100">
         <input  type="submit" value="修改" class='col-12 btn btn-success my-1'>
+                                                                           <!-- 在按鈕標籤中直接觸發onclick事件，並在事件發生時執行：
+                                                                                1.使用jquery來切換畫面上各區塊的顯示狀態 -->
         <input  type="button" value="回上頁" class='col-12 btn btn-secondary my-1' onclick="$('.list').show();$('.edit,.add').hide()">
     </div>
     </form>
@@ -62,11 +75,18 @@
 
 
  <script>
+    //前端編輯資料用的函式
     function edit(id){
+
+        //使用getJSON向後端api get_bus.php發出取得資料的請求
         $.getJSON('./api/get_bus.php',{id},(bus)=>{
-            console.log(bus)
+            //api 回傳的資料會是一個json格式的物件
+
+            //將bus物件中的name資料寫入到頁面上id為title的tag中
             $("#title").html(bus.name);
-            $("#minute2").val(bus.minute);
+
+            //將bus物件中的minute資料寫入到頁面上id為editMinute的input欄位的值
+            $("#editMinute").val(bus.minute);
         })
     }
  </script>
--- api/login.php
+++ api/login.php
@@ -6,9 +6,14 @@ $sql="select count(*) from `admin` where `acc`='{$_POST['acc']}' && `pw`='{$_POS
 $chk=$pdo->query($sql)->fetchColumn();
 
 if($chk==1){
+
+    //如果帳密正確，則建立一個$_SESSION['login']變數來做為登入的依據
     $_SESSION['login']=$_POST['acc'];
+
+    //如果登入成功，則將使用者導向後台頁面
     header("location:../admin.php");
 }else{
+    //如果登入失敗，則將使用者導向登入頁面，並在網址上加入錯誤碼
     header("location:../login.php?error=1");
 }
 
