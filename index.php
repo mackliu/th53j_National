8f9b7c5b462b519e2c158024eea4176acc96ea26
mackliu <macklun@gmail.com>
2023-05-27 11:18:22 +0800

    前台取出資料並拆成三維陣列進行資料排序處理

--- index.php
+++ index.php
@@ -7,14 +7,53 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>南港展覽館接駁專車-路網圖</title>
     <link rel="stylesheet" href="./bootstrap/bootstrap.css">
+    <style>
 
+        .block{
+            width:200px;
+            height:100px;
+            border:1px solid #ccc;
+
+        }
+    </style>
 </head>
 <body>
 <?php include "header.php";?>
+<div class="d-flex flex-wrap " style="width:600px">
+<?php 
+$sql="select * from `station`";
+$stations=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+$tmp=[];
+foreach($stations as $key => $station){
+    $tmp[floor($key/3)][]=$station;
+}
+/* echo "<pre>";
+print_r($tmp);
+echo "</pre>"; */
+
+foreach($tmp as $k => $t){
+    if($k%2==1){
+        krsort($t);
+        $tmp[$k]=$t;
+    }
+}
+
+/* echo "<pre>";
+print_r($tmp);
+echo "</pre>"; */
+
+foreach($tmp as $t){
+    foreach($t as $station){
+        echo "<div class='block'>";
+        echo $station['name'];
+        echo "</div>";
+    }
+}
 
 
+?>
 
-
+</div>
 <script src="./jquery/jquery.js"></script>
 <script src="./bootstrap/bootstrap.js"></script>
 </body>
