a7dd978843d3a925128d6bee53354f35a9d5e273
mackliu <macklun@gmail.com>
2023-05-28 16:12:12 +0800

    1. 增加程式碼註解 2. 解決已到站時仍會顯示其接駁車到站時間的問題

--- api/add_station.php
+++ api/add_station.php
@@ -1,10 +1,12 @@
 <?php include_once "db.php";
 
+//建立新增站點SQL語法
 $sql="insert into `station` (`name`,`minute`,`waiting`) 
                        values('{$_POST['name']}',
                              '{$_POST['minute']}',
                              '{$_POST['waiting']}')";
-
+//執行新增站點SQL語法
 $pdo->exec($sql);
 
+//新增完畢返回後台的站點管理頁面
 header("location:../admin.php?pos=station");
--- index.php
+++ index.php
@@ -9,6 +9,9 @@
     <link rel="stylesheet" href="./bootstrap/bootstrap.css">
     <style>
 
+        /*
+         * 建立一個站點的基本外框
+         */
         .block{
             width:200px;
             height:100px;
@@ -21,6 +24,8 @@
 
 
         }
+        
+        /**建立站點的外型圓點 */
         .point{
             width:25px;
             height:25px;
@@ -32,6 +37,8 @@
             position:relative;
             z-index:10;
         }
+
+        /**使用::before特性來建立圓點中的白圈 */
         .point::before{
             content:"";
             width:20px;
@@ -39,6 +46,8 @@
             border:3px solid white;
             border-radius:50%;
         }
+
+        /**建立一個只畫右邊直線的class */
         .right::after{
             content:"";
             width:50%;
@@ -47,6 +56,8 @@
             position:absolute;
             right:0;
         }
+
+        /**建立一個只畫左邊直線的class */
         .left::after{
             content:"";
             width:50%;
@@ -55,6 +66,8 @@
             position:absolute;
             left:0;
         }
+
+        /**建立一個畫左右直線的class */
         .line::after{
             content:"";
             width:100%;
@@ -63,16 +76,22 @@
             position:absolute;
             left:0;
         }
+
+        /**建立一個畫垂直線的class */
         .connect{
             width:18px;
             height:100px;
             background:skyblue;
             top:50%;
         }
+
+        /**建立一個讓直線靠右邊放置的class */
         .connect-right{
             position:absolute;
             right:0;
         }
+
+        /**建立一個讓直線靠左邊放置的class */
         .connect-left{
             position:absolute;
             left:0;
@@ -83,22 +102,35 @@
 <?php include "header.php";?>
 <div class="d-flex flex-wrap " style="width:600px">
 <?php 
+
+//取出所有的站點資料
 $sql="select * from `station`";
 $stations=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+//建立一個空陣列用來存放每個站點的應到時間及離開時間
+//時間的計算原理是每一站去累加行駛時間及停留時間
 $timer=[];
-$arrive=0;
-$leave=0;
+$arrive=0;  //初始到站時間為0
+$leave=0;   //初始離站時間為0
 foreach($stations as $station){
+
+    //到站時間為前一站的離站時間加上前一站到此站的行駛時間
     $arrive=$leave+$station['minute'];
+
+    //離開時間為此站的到達時間加上停留時間
     $leave=$arrive+$station['waiting'];
 
+    //以站點名稱為key名，到站時間為值存入$timer陣列中
     $timer[$station['name']]['arrive']=$arrive;
+
+    //以站點名稱為key名，離站時間為值存入$timer陣列中
     $timer[$station['name']]['leave']=$leave;
 }
 /*  echo "<pre>"; 
  print_r($timer); 
  echo "</pre>";  */
 
+ //建立一個暫存陣列，用來將站點以３站為一組做分組並存入暫存陣列中
 $tmp=[];
 foreach($stations as $key => $station){
     $tmp[floor($key/3)][]=$station;
@@ -107,7 +139,10 @@ foreach($stations as $key => $station){
  print_r($tmp); 
  echo "</pre>";  */ 
 
+//使用迴圈來檢視暫存陣列中每一組站點
 foreach($tmp as $k => $t){
+
+    //其中索引值為奇數的站點會進行反序的處理
     if($k%2==1){
         $tmp[$k]=array_reverse($t);
     }
@@ -121,14 +156,20 @@ echo "</pre>"; */
 $sql="select * from `bus` ";
 $buses=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
+
 foreach($tmp as $key => $t){
+
+    //判斷暫存陣列中的站點分組索引值為奇數或偶數，給予靠左或靠右的排列css
     if($key%2==1){
         echo "<div class='d-flex w-100 justify-content-end position-relative'>";
     }else{
         echo "<div class='d-flex w-100 position-relative'>";
     }
 
+    //根據站點總數來判斷是否要加上垂直連接線
     if((ceil(count($stations)/3)-1)>$key){
+
+        //判斷暫存陣列中的站點分組索引值為奇數或偶數，給予垂直連接線靠左或靠右的排列css
         if($key%2==1){
             echo "<div class='connect connect-left'></div>";
         }else{
@@ -136,19 +177,28 @@ foreach($tmp as $key => $t){
         }
     }
 
+    //將分組中的各個站點進行顯示相關的處理
     foreach($t as $k => $station){
 
+        //如果為起始站，則只畫右邊線
         if($key==0 && $k==0 ){
             echo "<div class='block right'>";
+
+            //如果為最後一站，需進一步判斷是那一個方向的最後一站
         }else if($key==ceil(count($stations)/3)-1){
        
+            //判斷分組索引值為奇數或偶數
             if($key%2==0){
+
+                //如果結束在偶數的橫列，則最後站為陣列的最後一個值，只需畫左邊線
                 if($k==count($t)-1){
                     echo "<div class='block left'>";
                 }else{
                     echo "<div class='block line'>";        
                 }
             }else{
+
+                //如果結束在奇數的橫列，則最後站為陣列的第一個值，只需畫右邊線
                 if($k==0){
                     echo "<div class='block right'>";
                 }else{
@@ -159,33 +209,58 @@ foreach($tmp as $key => $t){
             echo "<div class='block line'>";
         }
         
+        //建立一個用來暫存接駁車與站點到達時間間隔的陣列
+        //此陣列內用來判斷每一部接駁車離各站還有多少時間到達,
         $gap=[];
+
+        //建立一個註記用的變數，用來判斷此站點是否有接駁車已到站
+        //如果有接駁車已到站，則優先顯示已到站的接駁車資訊，
+        //未到站的接駁車則不顯示
+        $flag=0;
+
+        //巡訪每一部接駁車，計算接駁車和此站點的時間關係
+        //接駁車已行駛時間 < 站點到達時間 < 0 => 未到站
+        //接駁車已行駛時間 >= 站點到達時間  && 接駁車已行駛時間 <= 站點離開時間 >= 0 => 已到站
+        //接駁車已行駛時間 > 站點離開時間 => 已離站
         foreach($buses as $bus){
-            if($bus['minute']-$timer[$station['name']]['arrive']>=0 && $bus['minute']-$timer[$station['name']]['leave']<=0){
+            if(($bus['minute'] >= $timer[$station['name']]['arrive']) && ($bus['minute'] <= $timer[$station['name']]['leave'])){
+                //接駁車已到站，顯示到站資訊
                 echo $bus['name'];
                 echo "已到站";
-            }else if($bus['minute']-$timer[$station['name']]['arrive']<0 ){
+
+                //將$flag設為1，表示此車為已到站狀態
+                $flag=1;
+            }else if($bus['minute'] < $timer[$station['name']]['arrive']){
+
+                //接駁車未到站，將到站時間存入gap陣列中，其中接駁車名為鍵名
                 $gap[$bus['name']]=abs($bus['minute']-$timer[$station['name']]['arrive']);
             }
         }
-        if(!empty($gap)){
+
+        //每一部接駁車巡訪完畢後檢視gap陣列中是否有值，及接駁車是否已到站
+        //當此站點無已到站的接駁車時，從gap陣列中，取出到站時間最短的接駁車資料
+        if(!empty($gap) && $flag==0){
             $min=min($gap);
-            $name=array_search($min,$gap);
+            $name=array_search($min,$gap); //利用array_search()來找到目標值的鍵值(接駁車名)
             echo $name ;
             echo "約".$min."分鐘";
+
+            //清空gap陣列內容，讓下一個迴圈重新使用$gap這個變數
+            $gap=[];
+
+            //重設$flag值為0，讓下一個迴圈重新使用$flag這個變數
+            $flag=0;
         }
         
-        $gap=[];
 
 
+        //顯示此站點名稱
         echo "<div class='point'></div>";
         echo $station['name'];
         echo "</div>";
     }
     echo "</div>";
 }
-
-
 ?>
 
 </div>
