a7674ad338692d5293d0f244e4b08890b875501f
mackliu <macklun@gmail.com>
2023-06-01 17:07:55 +0800

    另一種解法測試

--- index.php
+++ index.php
@@ -137,128 +137,56 @@ foreach($stations as $key => $station){
 /*   echo "<pre>"; 
  print_r($tmp); 
  echo "</pre>";  */ 
-
+$stations=[]; //清空原本的站點陣列
 //使用迴圈來檢視暫存陣列中每一組站點
 foreach($tmp as $k => $t){
 
     //其中索引值為奇數的站點會進行反序的處理
     if($k%2==1){
-        $tmp[$k]=array_reverse($t);
+        $t=array_reverse($t);
     }
+
+    $stations = array_merge($stations,$t); //將排序後的陣列組合回原本陣列
 }
 
 /* echo "<pre>";
-print_r($tmp);
+print_r($stations);
 echo "</pre>"; */
+
 
 //所有的車子資料撈出來
 $sql="select * from `bus` ";
 $buses=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
+$direction="right";//預設路網起始為向右
 
-foreach($tmp as $key => $t){
-
+foreach($stations as $key => $stations){
+    $group=floor($key/3);
     //判斷暫存陣列中的站點分組索引值為奇數或偶數，給予靠左或靠右的排列css
-    if($key%2==1){
-        echo "<div class='d-flex w-100 justify-content-end position-relative'>";
-    }else{
-        echo "<div class='d-flex w-100 position-relative'>";
-    }
+    // 餘1為靠右(justify-content-end)
+    // 餘0為靠左(flex預設靠左)
+    if(($key % 3==0)){
 
-    //根據站點總數來判斷是否要加上垂直連接線
-    if((ceil(count($stations)/3)-1)>$key){
+        if($group%2==1){
+           echo "<div class='d-flex w-100 justify-content-end position-relative'>";
+           $direction="left";  //將路網走向設為向左
+       }else{
+           echo "<div class='d-flex w-100 position-relative'>";
+           $direction="right";  //將路網走向設為向右
+       }
 
-        //判斷暫存陣列中的站點分組索引值為奇數或偶數，給予垂直連接線靠左或靠右的排列css
-        if($key%2==1){
-            echo "<div class='connect connect-left'></div>";
-        }else{
-            echo "<div class='connect connect-right'></div>";
-        }
-    }
-
-    //將分組中的各個站點進行顯示相關的處理
-    foreach($t as $k => $station){
-
-        //如果為起始站，則只畫右邊線
-        if($key==0 && $k==0 ){
-            echo "<div class='block right'>";
-
-            //如果為最後一站，需進一步判斷是那一個方向的最後一站
-        }else if($key==ceil(count($stations)/3)-1){
        
-            //判斷分組索引值為奇數或偶數
-            if($key%2==0){
+       
+    }
+    echo "<div class='block'>";
 
-                //如果結束在偶數的橫列，則最後站為陣列的最後一個值，只需畫左邊線
-                if($k==count($t)-1){
-                    echo "<div class='block left'>";
-                }else{
-                    echo "<div class='block line'>";        
-                }
-            }else{
+    echo "<div class='point'></div>";
 
-                //如果結束在奇數的橫列，則最後站為陣列的第一個值，只需畫右邊線
-                if($k==0){
-                    echo "<div class='block right'>";
-                }else{
-                    echo "<div class='block line'>";
-                }
-            }
-        }else{
-            echo "<div class='block line'>";
-        }
-        
-        //建立一個用來暫存接駁車與站點到達時間間隔的陣列
-        //此陣列內用來判斷每一部接駁車離各站還有多少時間到達,
-        $gap=[];
-
-        //建立一個註記用的變數，用來判斷此站點是否有接駁車已到站
-        //如果有接駁車已到站，則優先顯示已到站的接駁車資訊，
-        //未到站的接駁車則不顯示
-        $flag=0;
-
-        //巡訪每一部接駁車，計算接駁車和此站點的時間關係
-        //接駁車已行駛時間 < 站點到達時間 < 0 => 未到站
-        //接駁車已行駛時間 >= 站點到達時間  && 接駁車已行駛時間 <= 站點離開時間 >= 0 => 已到站
-        //接駁車已行駛時間 > 站點離開時間 => 已離站
-        foreach($buses as $bus){
-            if(($bus['minute'] >= $timer[$station['name']]['arrive']) && ($bus['minute'] <= $timer[$station['name']]['leave'])){
-                //接駁車已到站，顯示到站資訊
-                echo $bus['name'];
-                echo "已到站";
-
-                //將$flag設為1，表示此車為已到站狀態
-                $flag=1;
-            }else if($bus['minute'] < $timer[$station['name']]['arrive']){
-
-                //接駁車未到站，將到站時間存入gap陣列中，其中接駁車名為鍵名
-                $gap[$bus['name']]=abs($bus['minute']-$timer[$station['name']]['arrive']);
-            }
-        }
-
-        //每一部接駁車巡訪完畢後檢視gap陣列中是否有值，及接駁車是否已到站
-        //當此站點無已到站的接駁車時，從gap陣列中，取出到站時間最短的接駁車資料
-        if(!empty($gap) && $flag==0){
-            $min=min($gap);
-            $name=array_search($min,$gap); //利用array_search()來找到目標值的鍵值(接駁車名)
-            echo $name ;
-            echo "約".$min."分鐘";
-
-            //清空gap陣列內容，讓下一個迴圈重新使用$gap這個變數
-            $gap=[];
-
-            //重設$flag值為0，讓下一個迴圈重新使用$flag這個變數
-            $flag=0;
-        }
-        
-
-
-        //顯示此站點名稱
-        echo "<div class='point'></div>";
-        echo $station['name'];
+    echo "</div>";
+    if( $key%3 == 2){
         echo "</div>";
     }
-    echo "</div>";
+
 }
 ?>
 
