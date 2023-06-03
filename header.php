89d97c51143be3f031385559fc332ff8cec05841
mackliu <macklun@gmail.com>
2023-06-03 15:05:14 +0800

    增加登出功能

--- header.php
+++ header.php
@@ -4,6 +4,14 @@
    <a href="index.php">南港展覽館接駁專車系統</a>
 </div>
 <div class="col-6 row align-items-center justify-content-end">
-    <a href="admin.php">系統管理</a>
+    <a href="admin.php" class="mx-2">系統管理</a>
+    <?php
+    if(isset($_SESSION['login'])){
+        echo "<a href='./api/logout.php'  class='mx-2'>登出</a>";
+
+    }
+
+?>
+
 </div>
 </header>
