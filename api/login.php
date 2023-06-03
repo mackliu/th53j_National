8c6bcbf9dffc9c70770fa293a4cb198b3e125659
mackliu <macklun@gmail.com>
2023-06-03 15:18:01 +0800

    加入登入驗證碼檢查

--- api/login.php
+++ api/login.php
@@ -1,6 +1,14 @@
 <?php
 include_once "db.php";
 
+if($_POST['code']!=$_SESSION['code']){
+    header("location:../login.php?error=2");
+    exit();
+}
+
+
+
+
 $sql="select count(*) from `admin` where `acc`='{$_POST['acc']}' && `pw`='{$_POST['pw']}'";
 
 $chk=$pdo->query($sql)->fetchColumn();
