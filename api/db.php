11bd5cf6cca5487d04af17e1d71bdf3d4e5166b1
mackliu <macklun@gmail.com>
2023-07-01 15:45:05 +0800

    把站點和接駁車的關係獨立成一個function來處理

--- api/db.php
+++ api/db.php
@@ -5,4 +5,94 @@ $pdo=new PDO($dsn,'root','');
 
 session_start();
 
+function get_station($id){
+    global $pdo;
+    
+    //撈出站點資料
+    $station=$pdo->query("select * from `station` where `id`='$id'")->fetch(PDO::FETCH_ASSOC);
+    
+    //計算站點前的所有站點行駛時間總計(到達時間＋停等時間)
+    $prev_time=$pdo->query("select sum(`minute`+`waiting`) from `station` where `before`< {$station['before']}")->fetchColumn();
+    
+    //計算當前站點的到站時間
+    $arrive=$prev_time+$station['minute'];
+    
+    //計算當前站點的離站時間
+    $leave=$arrive+$station['waiting'];
+    
+    //撈出所有的接駁車資訊
+    $buses=$pdo->query("select * from bus")->fetchAll(PDO::FETCH_ASSOC);
+    
+    //巡訪每一部接駁車，計算接駁車和此站點的時間關係
+    //建立一個陣列用來儲存每部接駁車目前和此一站點的到站時間及離站時間差距
+    $busInfo=[];
+    
+    foreach($buses as $bus){
+        $bus['arrive']=$bus['minute']-$arrive;
+        $bus['leave']=$bus['minute']-$leave;
+        if($bus['arrive']<0){
+            $bus['status']='wait';
+            $bus['show']='約'.abs($bus['arrive']).'分鐘';
+        }else if($bus['arrive']>=0 && $bus['leave'] <=0){
+            $bus['status']='arrive';
+            $bus['show']='已到站';
+        }else{
+            $bus['status']='passed';
+            $bus['show']='已過站';
+        }
+    
+        $busInfo[$bus['name']]=$bus;
+    }   
+    
+    //建立一個二維陣列，依，己到站，未到站，已過站順序分類接駁車
+    $sortBus=[
+        0=>[],
+        1=>[],
+        2=>[],
+    ];
+    
+    //將接駁車分類到三個不同的陣列進行不同的處理
+    foreach($busInfo as $name => $bus){
+    
+        switch($bus['status']){
+            case 'arrive':
+                $sortBus[0][]=$bus;
+            break;
+            case 'wait':
+                $sortBus[1][]=$bus;
+            break;
+            case 'passed':
+                $sortBus[2][]=$bus;
+            break;
+        }
+    }
+    
+    /**
+     * 排序接駁車：
+     * 已到站：應該是只有一筆，而且會在陣列最前面
+     * 未到站：依大->小排序，表示快到站的時間
+     * 已過站：依小->大排序，表示離站的時間
+     */
+    
+     if(!empty($sortBus[1])){
+        uasort($sortBus[1],function($a,$b){
+            return $b['arrive'] - $a['arrive'];
+        });
+     }
+    
+    if(!empty($sortBus[2])){
+        uasort($sortBus[2],function($a,$b){
+            return $a['leave'] - $b['leave'];
+        });  
+    }
+
+    $busInfo=[];
+    
+    foreach($sortBus as $group){
+        $busInfo=array_merge($busInfo,$group);
+    }
+    
+    return ['name'=>$station['name'],
+            'buses'=>$busInfo];
+        }
 ?>
--- index.php
+++ index.php
@@ -131,6 +131,7 @@
             border:1px solid #555;
             border-radius:50%;
             margin:5px;
+            cursor: pointer;
         }
         .active{
             background:blue;
@@ -163,73 +164,32 @@
 $sql="select * from `station` order by `before`";
 $stations=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
-//建立一個空陣列用來存放每個站點的應到時間及離開時間
-//時間的計算原理是每一站去累加行駛時間及停留時間
-$timer=[];
-$arrive=0;  //初始到站時間為0
-$leave=0;   //初始離站時間為0
-
 $div=$_GET['p']??3;
-//$div=isset($_GET['p'])?$_GET['p']:3;
 
-foreach($stations as $station){
 
-    //到站時間為前一站的離站時間加上前一站到此站的行駛時間
-    $arrive=$leave+$station['minute'];
-
-    //離開時間為此站的到達時間加上停留時間
-    $leave=$arrive+$station['waiting'];
-
-    //以站點名稱為key名，到站時間為值存入$timer陣列中
-    $timer[$station['name']]['arrive']=$arrive;
-
-    //以站點名稱為key名，離站時間為值存入$timer陣列中
-    $timer[$station['name']]['leave']=$leave;
+//建立一個分組陣列，用來將站點以$div站為一組做分組並存入暫存陣列中
+$group=[];
+foreach($stations as $idx => $station){
+    $group[floor($idx/$div)][]=$station;
 }
-/*  echo "<pre>"; 
- print_r($timer); 
- echo "</pre>";  */
-
- //建立一個暫存陣列，用來將站點以$div站為一組做分組並存入暫存陣列中
-$tmp=[];
-foreach($stations as $key => $station){
-    $tmp[floor($key/$div)][]=$station;
-}
-/*   echo "<pre>"; 
- print_r($tmp); 
- echo "</pre>";  */ 
 
 //使用迴圈來檢視暫存陣列中每一組站點
-foreach($tmp as $k => $t){
+foreach($group as $idx => $points){
 
     //其中索引值為奇數的站點會進行反序的處理
-    if($k%2==1){
-        $tmp[$k]=array_reverse($t);
+    if($idx%2==1){
+        $group[$idx]=array_reverse($points);
     }
 }
-
-/* echo "<pre>";
-print_r($tmp);
-echo "</pre>"; */
-
-//所有的車子資料撈出來
-$sql="select * from `bus` ";
-$buses=$pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 
 //$tmp是站點分組的陣列
 //$group是分組的索引值
 //$g則是分組中的站點陣列
-foreach($tmp as $group => $g){
+foreach($group as $idx => $points){
 
     //判斷暫存陣列中的站點分組索引值為奇數或偶數，給予靠左或靠右的排列css
-    /**
-     *   -------   0 -> $group
-     *          |
-     *   -------   1 -> $group
-     *  |
-     *   -------   2 -> $group
-     */
-    if($group%2==1){
+
+    if($idx%2==1){
         echo "<div class='d-flex w-100 justify-content-end position-relative'>";
     }else{
         echo "<div class='d-flex w-100 position-relative'>";
@@ -240,11 +200,11 @@ foreach($tmp as $group => $g){
      * 分組數目->count($tmp)
      * 檢查值->分組數目-1 //因為要從0開始算
      */
-    $chk=count($tmp)-1;
-    if($chk>$group){
+    $chk=count($group)-1;
+    if($chk>$idx){
 
         //判斷暫存陣列中的站點分組索引值為奇數或偶數，給予垂直連接線靠左或靠右的排列css
-        if($group%2==1){
+        if($idx%2==1){
             echo "<div class='connect connect-left'></div>";
         }else{
             echo "<div class='connect connect-right'></div>";
@@ -252,27 +212,27 @@ foreach($tmp as $group => $g){
     }
 
     //將分組中的各個站點進行顯示相關的處理，主要是用來決定要畫那種線
-    foreach($g as $idx => $station){
+    foreach($points as $num => $station){
 
         //如果為起始站，則只畫右邊線
-        //起始站的定義是 分組($group)為第一組 而且 在第一組中的第一站(索引為0)
-        if($group==0 && $idx==0 ){
+        //起始站的定義是 分組($idx)為第一組 而且 在第一組中的第一站(索引為0)
+        if($idx==0 && $num==0 ){
             echo "<div class='block right'>";
 
             //如果為最後一站，需進一步判斷是那一個方向的最後一站
             //最後一站的定義是 分組($group)為最後一組 
             //最後一組的判斷法為$tmp的最後一個元素的索引值(count($tmp)-1)
             //
-        }else if($group==(count($tmp)-1)){
+        }else if($idx==(count($group)-1)){
        
             //判斷分組索引值為偶數，而且站點為目前分組的最後一個
-            if($group%2==0 && ($idx==count($g)-1)){
+            if($idx%2==0 && ($num==count($points)-1)){
                 
                     //只需畫左邊的線
                     echo "<div class='block left'>";
 
             //判斷分組索引值為奇數，而且站點為目前分組的第一個
-            }else if($group%2==1 && $idx==0){
+            }else if($idx%2==1 && $num==0){
             
                     //只需畫右邊的線
                     echo "<div class='block right'>";
@@ -281,126 +241,38 @@ foreach($tmp as $group => $g){
                 echo "<div class='block line'>";
             }
         }else{
-
             echo "<div class='block line'>";
         }
 
-        //巡訪每一部接駁車，計算接駁車和此站點的時間關係
-        //建立一個陣列用來儲存每部接駁車目前和此一站點的到站時間及離站時間差距
-        $busInfo=[];
-        foreach($buses as $bus){
-            $busInfo[$bus['name']]['arrive']=$bus['minute']-$timer[$station['name']]['arrive'];
-            $busInfo[$bus['name']]['leave']=$bus['minute']-$timer[$station['name']]['leave'];
-        }   
-
-        //建立一個暫用的陣列，用來儲存距離此站點到站時間最短的接駁，預設給一個極小值
-        $min=['min'=>-999999,'bus'=>""];
-        $flag=0;  //建立一個標記，用來標記已到站的接駁車
-
-        //巡訪每一部接駁車的資訊,依以下原則進行狀態的紀錄
-        //接駁車已行駛時間 < 站點到達時間 < 0 => 未到站
-        //接駁車已行駛時間 >= 站點到達時間  && 接駁車已行駛時間 <= 站點離開時間 >= 0 => 已到站
-        //接駁車已行駛時間 > 站點離開時間 => 已離站        
-        foreach($busInfo as $bus => $info){
-            if($info['arrive']>=0 && $info['leave']<=0){
-                $busInfo[$bus]['status']="已到站";
-
-                //判斷是否有車為已到站，如果有則加上flag，
-                //如果同時有多部車為已到站的情形，則依照flag的狀態
-                //只需顯示一筆已到站的訊息即可
-                if($flag!=1){
-                    echo "<div class='block-top arrive'>";
-                    echo $bus . "<br>已到站";
-                    echo "</div>";
-                    $flag=1;
-                }
-            }else if($info['leave']>0){
-                $busInfo[$bus]['status']="已過站";
-            }else{
-                $busInfo[$bus]['status']="約".abs($info['arrive'])."分鐘";
-
-                //如果接駁車為未到站的狀態，則更新$min中的最接近車輛資訊
-                if($info['arrive']>$min['min']){
-                    $min['min']=$info['arrive'];
-                    $min['bus']=$bus;
-                }
-            }
+        //顯示接駁車資訊
+        $station_buses=get_station($station['id']);
+       //print_r($station_buses);
+        echo "<div class='block-top {$station_buses['buses'][0]['status']}'>";
+        if($station['before']==0 && $station_buses['buses'][0]['status']=='passed'){
+            echo "<span style='color:gray'>未發車</span>";
+        }else{
+            echo $station_buses['buses'][0]['name'];
+            echo "<br>";
+            echo $station_buses['buses'][0]['show'];
         }
-
-        //判斷未到站及$min的資訊有被更新過
-        if($flag==0 && $min['bus']!=""){
-            echo "<div class='block-top'>";
-            echo $min['bus'] . "<br>";
-            echo "約".abs($min['min'])."分鐘";
-            echo "</div>";
-        }
-
-        //判斷首站無發車的狀態
-        if($flag==0 && $min['bus']==""){
-            echo "<div class='block-top' style='color:#999'>";
-            echo "未發車";
-            echo "</div>"; 
-        }
-
-        /**
-         * 可選功能:
-         * 除了直接印出陣列中的前三部接駁車的做法外，
-         * 此做法為將接駁車分為已到站，未到站，已過站三個次陣列
-         * 然後依照已到站＞未到站＞已過站的順序，選三部接駁車的資訊放入到彈出框中顯示
-         */
-
-         //將接駁車的狀態依照已到站、未到站、已過站三種狀態分成三個陣列
-        $infoTmp=[];
-        foreach($busInfo as $bus => $info){
-            if($info['status']=="已到站"){
-                $infoTmp['已到站'][$bus]=$info;
-            }else if($info['status']=='已過站'){
-                $infoTmp['已過站'][$bus]=$info;
-            }else{
-                $infoTmp['未到站'][$bus]=$info;
-            }
-        }
-
-        //建立一個陣列用來儲存最後要顯示在頁面上的三部接駁車資訊
-        $busList=[];
-        
-        //增加一個變數來決定要顯示的車子資訊數量
-        //最多三筆
-        $maxBus=(count($buses)>=3)?3:count($buses);
-
-        //依照已到站,未到站,已過站的順序將接駁車資料放入到$busList陣列中
-        //使用while迴圈來執行選車的動作直到$busList中的接駁車滿三部為止
-        while(count($busList)<$maxBus){
-            if(!empty($infoTmp['已到站'])){
-                $busList[array_keys($infoTmp['已到站'])[0]]=array_shift($infoTmp['已到站']);
-            }else if(!empty($infoTmp['未到站'])){
-                $busList[array_keys($infoTmp['未到站'])[0]]=array_shift($infoTmp['未到站']);
-            }else{
-                $busList[array_keys($infoTmp['已過站'])[0]]=array_shift($infoTmp['已過站']);
-            }
-        }
+        echo "</div>";
 
         //顯示站點圈圈
         echo "<div class='point'></div>";
 
         //建立一個容器用來放三輛接駁車資訊,此容器預設為不顯示(display:none)
         echo "<div class='bus-info'>";
-        //$count=0;  //建立一個計數器，當迴圈跑完前三次時就停止
-        //foreach($busInfo as $name => $info){ //只取前三車時的寫法
-        foreach($busList as $name => $info){
-            //if($count<3){
-                if($info['status']=='已到站'){
+     
+        for($i=0;$i<3;$i++){
+                if($station_buses['buses'][$i]['status']=='arrive'){
 
                     //已到站的車以紅字顯示
                     echo "<div class='arrive'>";
-
                 }else{
                     echo "<div>";
                 }
-                echo $name.": ".$info['status'];
+                echo $station_buses['buses'][$i]['name'].": ".$station_buses['buses'][$i]['show'];
                 echo "</div>";
-            //}
-           // $count++;
         }
         echo "</div>";
 
@@ -421,17 +293,15 @@ foreach($tmp as $group => $g){
     //當滑鼠移到站點圈圈上時，顯示接駁車資訊彈出視窗
     $(".point").hover(
         function(){
-            $(this).next().show();
+            $(this).next().show();      
         },
         //當滑鼠移出站點圈圈時，隱藏接駁車資訊彈出視窗
         function(){
             $(".block .bus-info").hide();
         }
     )
-
     function showStation(num){
         location.href='?p='+num;
     }
-
 
 </script>
